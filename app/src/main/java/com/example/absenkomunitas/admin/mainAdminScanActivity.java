9ff576a1c80c6c4b4e6aaf9b5ba358130b9e3b49
andrmhndr <66835454+andrmhndr@users.noreply.github.com>
2021-02-16 15:58:57 +0700

    admin activity

--- app/src/main/java/com/example/absenkomunitas/admin/mainAdminScanActivity.java
+++ app/src/main/java/com/example/absenkomunitas/admin/mainAdminScanActivity.java
@@ -1,21 +1,37 @@
 package com.example.absenkomunitas.admin;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
 import com.example.absenkomunitas.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.zxing.Result;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class mainAdminScanActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseFirestore db;
+
+    private CodeScanner scanner;
+    private CodeScannerView scannerView;
 
     private Button btnBack;
 
@@ -23,6 +39,26 @@ public class mainAdminScanActivity extends AppCompatActivity {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_qr_scan);
+
+        scannerView = findViewById(R.id.scanner_view);
+        scanner = new CodeScanner(this, scannerView);
+        scanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(mainAdminScanActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+        scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scanner.startPreview();
+            }
+        });
 
         btnBack = findViewById(R.id.btnBack);
         btnBack.setOnClickListener(new View.OnClickListener() {
@@ -37,10 +73,42 @@ public class mainAdminScanActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        requestForCamera();
+        scanner.startPreview();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        scanner.releaseResources();
+    }
+
+    @Override
     public void onBackPressed() {
         super.onBackPressed();
         Intent goMainAdmin = new Intent(mainAdminScanActivity.this, mainAdminActivity.class);
         startActivity(goMainAdmin);
         finish();
+    }
+
+    private void requestForCamera() {
+        Dexter.withActivity(this).withPermission(Manifest.permission.CAMERA).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                scanner.startPreview();
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                Toast.makeText(mainAdminScanActivity.this, "Membutuhkan persetujuan Kamera.", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                permissionToken.continuePermissionRequest();
+            }
+        }).check();
     }
 }
--- app/src/main/java/com/example/absenkomunitas/user/mainUserQRActivity.java
+++ app/src/main/java/com/example/absenkomunitas/user/mainUserQRActivity.java
@@ -1,40 +1,31 @@
 package com.example.absenkomunitas.user;
 
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.util.Log;
 import android.view.View;
-import android.view.inputmethod.InputMethod;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.absenkomunitas.R;
 import com.example.absenkomunitas.model.modelUser;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
-import com.google.zxing.common.BitMatrix;
-import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
 
 public class mainUserQRActivity extends AppCompatActivity {
 
-    FirebaseAuth mAuth;
+    private FirebaseAuth mAuth;
+    private QRGEncoder qrgEncoder;
 
     private Button btnBack;
     private ImageView QRCode;
@@ -65,16 +56,11 @@ public class mainUserQRActivity extends AppCompatActivity {
         FirebaseUser user = mAuth.getCurrentUser();
         userModel.setUid(user.getUid());
 
-        MultiFormatWriter writer = new MultiFormatWriter();
+        qrgEncoder = new QRGEncoder(userModel.getUid(), null, QRGContents.Type.TEXT, 200);
         try {
-            BitMatrix matrix = writer.encode(user.getUid(), BarcodeFormat.QR_CODE, 200,200);
-            BarcodeEncoder encoder = new BarcodeEncoder();
-            Bitmap bitmap = encoder.createBitmap(matrix);
-            QRCode.setImageBitmap(bitmap);
-            InputMethodManager manager = (InputMethodManager) getSystemService(
-                    Context.INPUT_METHOD_SERVICE
-            );
-        } catch (WriterException e){
+            Bitmap qrBits = qrgEncoder.getBitmap();
+            QRCode.setImageBitmap(qrBits);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
